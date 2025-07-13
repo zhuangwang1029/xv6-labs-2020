cd8344a0534a493657d383753a8f3ed69de86773
zhuangwang1029 <1905212870@qq.com>
2025-07-13 19:52:34 +0800

    Implemented Eliminate allocation from sbrk()

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -43,12 +43,14 @@ sys_sbrk(void)
 {
   int addr;
   int n;
+  struct proc *p = myproc();
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  
+  addr = p->sz;
+  p->sz += n;  // 只增加进程的虚拟地址空间大小，不分配物理内存
+  
   return addr;
 }
 
