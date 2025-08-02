a499b6ff9df4ed05991b63779a54666254ae934a
zhuangwang1029 <1905212870@qq.com>
2025-08-02 16:12:31 +0800

    Implemented Large files

--- kernel/fs.c
+++ kernel/fs.c
@@ -381,6 +381,7 @@ bmap(struct inode *ip, uint bn)
   struct buf *bp;
 
   if(bn < NDIRECT){
+    // 直接块：0-10
     if((addr = ip->addrs[bn]) == 0)
       ip->addrs[bn] = addr = balloc(ip->dev);
     return addr;
@@ -388,6 +389,7 @@ bmap(struct inode *ip, uint bn)
   bn -= NDIRECT;
 
   if(bn < NINDIRECT){
+    // 间接块：11-266 (256个块)
     // Load indirect block, allocating if necessary.
     if((addr = ip->addrs[NDIRECT]) == 0)
       ip->addrs[NDIRECT] = addr = balloc(ip->dev);
@@ -395,6 +397,37 @@ bmap(struct inode *ip, uint bn)
     a = (uint*)bp->data;
     if((addr = a[bn]) == 0){
       a[bn] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    return addr;
+  }
+  bn -= NINDIRECT;
+
+  if(bn < NDINDIRECT){
+    // 双重间接块：267-65802 (256*256个块)
+    // Load doubly-indirect block, allocating if necessary.
+    if((addr = ip->addrs[NDIRECT + 1]) == 0)
+      ip->addrs[NDIRECT + 1] = addr = balloc(ip->dev);
+    
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    
+    // 计算在双重间接块中的索引
+    uint indirect_idx = bn / NINDIRECT;  // 第几个间接块 (0-255)
+    uint direct_idx = bn % NINDIRECT;    // 在间接块中的索引 (0-255)
+    
+    if((addr = a[indirect_idx]) == 0){
+      a[indirect_idx] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    
+    // 现在访问间接块
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if((addr = a[direct_idx]) == 0){
+      a[direct_idx] = addr = balloc(ip->dev);
       log_write(bp);
     }
     brelse(bp);
@@ -409,10 +442,11 @@ bmap(struct inode *ip, uint bn)
 void
 itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+  int i, j, k;
+  struct buf *bp, *bp2;
+  uint *a, *a2;
 
+  // 释放直接块 (0-10)
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
       bfree(ip->dev, ip->addrs[i]);
@@ -420,6 +454,7 @@ itrunc(struct inode *ip)
     }
   }
 
+  // 释放间接块 (第11个地址)
   if(ip->addrs[NDIRECT]){
     bp = bread(ip->dev, ip->addrs[NDIRECT]);
     a = (uint*)bp->data;
@@ -430,6 +465,29 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  // 释放双重间接块 (第12个地址)
+  if(ip->addrs[NDIRECT + 1]){
+    bp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+    a = (uint*)bp->data;
+    for(j = 0; j < NINDIRECT; j++){
+      if(a[j]){
+        // 释放每个间接块中的数据块
+        bp2 = bread(ip->dev, a[j]);
+        a2 = (uint*)bp2->data;
+        for(k = 0; k < NINDIRECT; k++){
+          if(a2[k])
+            bfree(ip->dev, a2[k]);
+        }
+        brelse(bp2);
+        // 释放间接块本身
+        bfree(ip->dev, a[j]);
+      }
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+    ip->addrs[NDIRECT + 1] = 0;
   }
 
   ip->size = 0;
--- kernel/fs.h
+++ kernel/fs.h
@@ -24,9 +24,10 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
-#define NDIRECT 12
-#define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define NDIRECT 11    // 从12改为11，为双重间接块腾出空间
+#define NINDIRECT (BSIZE / sizeof(uint))            // 保持256不变
+#define NDINDIRECT (NINDIRECT * NINDIRECT)          // 新增：256*256 = 65536
+#define MAXFILE (NDIRECT + NINDIRECT + NDINDIRECT)  // 11 + 256 + 65536 = 65803
 
 // On-disk inode structure
 struct dinode {
@@ -35,7 +36,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEVICE only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+2];   // 从NDIRECT+1改为NDIRECT+2，增加双重间接块
 };
 
 // Inodes per block.
